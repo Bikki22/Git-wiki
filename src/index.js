d23e459af0328676facd6da4378c3f711a01c7cf
Bikki chaudhary <cbikki35@gmail.com>
2026-09-06 23:39:50 +0545

    feat: integrate Inngest for serverless functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import express from "express";
 import "dotenv/config";
+import express from "express";
+import { inngest, functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
 
 const app = express();
 
+const PORT = process.env.PORT ?? 8000;
+
+app.use(express.json());
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 app.get("/health", (req, res) => {
+  res.send("hello from health route");
+});
+app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.listen(PORT, () => {
+  console.log("server is running in PORT", PORT);
 });
